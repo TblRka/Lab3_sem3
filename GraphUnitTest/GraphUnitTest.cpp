1200c6c7330d2a395a05e9914e5c53be09d14878
TblRka <92474441+TblRka@users.noreply.github.com>
2023-01-20 16:18:48 +0300

    patch 0.5

--- GraphUnitTest/GraphUnitTest.cpp
+++ GraphUnitTest/GraphUnitTest.cpp
@@ -10,9 +10,9 @@ namespace GraphUnitTest
 	{
 	public:
 		
-		TEST_METHOD(GraphTest)
+		TEST_METHOD(UnderectedGraphTest)
 		{
-			Graph<int, int>* test_graph = new Graph<int, int>();
+			UndirectedGraph<int, int>* test_graph = new UndirectedGraph<int, int>();
 			for (int i = 1; i < 151; i++)
 			{
 				test_graph->add_vertex(i, 0);
@@ -28,9 +28,9 @@ namespace GraphUnitTest
 			test_graph->erase_vertex(5);
 
 			Assert::IsTrue((test_graph->get_vertex_count()) == 149, L"Get vertex 2");
-			Assert::IsTrue((test_graph->get_edge_count()) == 150 * (150 - 1) / 2 - 149, L"Get edge 1");
+			Assert::IsTrue((test_graph->get_edge_count()) == 150 * (150 - 1) / 2 - 149, L"Get edge 2");
 
-			Assert::IsTrue((test_graph->is_weighed()) == false, L"Get edge 1");
+			Assert::IsTrue((test_graph->is_weighed()) == false, L"Is weighed");
 
 
 		}
@@ -40,11 +40,15 @@ namespace GraphUnitTest
 			DirectedGraph<int, int>* test_graph = new DirectedGraph<int, int>();
 			for (int i = 1; i < 151; i++)
 			{
-				test_graph->add_vertex(i, );
+				srand(time(NULL));
+				test_graph->add_vertex(i, rand() % 100);
 				for (int j = 1; j < i; j++)
 				{
-					test_graph->add_edge(j, i, 0);
+					test_graph->add_edge(j, i, rand() % 100);
 				}
+
+				Assert::IsTrue((test_graph->is_weighed()) == true, L"Is weighed");
+
 			}
 		}
 	};
