12e77c79ef9b9c941cdb9a3968719eb14b03e914
TblRka <92474441+TblRka@users.noreply.github.com>
2023-01-21 18:54:50 +0300

    patch 1.7

--- DirectedGraph.h
+++ DirectedGraph.h
@@ -1,10 +1,12 @@
 #include "Graph.h"
 
 template <class TV, class TE>
-class DirectedGraph : public Graph<TV, TE> {
+class DirectedGraph : public Graph<TV, TE> 
+{
 private:
 	using Path = typename Graph<TV, TE>::Path;
-	Path bfs(size_t from, size_t to) {
+	Path bfs(size_t from, size_t to) 
+	{
 		Path path;
 		std::queue<size_t> q;
 		std::unordered_map<size_t, bool> checked(Graph<TV, TE>::vertex_count);
@@ -12,28 +14,35 @@ private:
 		q.push(from);
 		parents[from] = 0;
 		checked[from] = true;
-		while (!q.empty()) {
+		while (!q.empty()) 
+		{
 			size_t vert = q.front();
-			if (vert == to) {
+			if (vert == to) 
+			{
 				break;
 			}
 			q.pop();
-			for (auto it = Graph<TV, TE>::incidence_list[vert].begin(); it != Graph<TV, TE>::incidence_list[vert].end(); it++) {
-				if (checked.find(it->id_to) == checked.end()) {
+			for (auto it = Graph<TV, TE>::incidence_list[vert].begin(); it != Graph<TV, TE>::incidence_list[vert].end(); it++) 
+			{
+				if (checked.find(it->id_to) == checked.end()) 
+				{
 					checked[it->id_to] = true;
 					q.push(it->id_to);
 					parents[it->id_to] = vert;
 				}
 			}
 		}
-		if (checked.find(to) == checked.end()) {
+		if (checked.find(to) == checked.end()) 
+		{
 			return path;
 		}
 		size_t child = to;
 		size_t cur_parent = parents[child];
 		while (cur_parent != 0) {
-			for (auto it = Graph<TV, TE>::incidence_list[child].begin(); it != Graph<TV, TE>::incidence_list[child].end(); it++) {
-				if (cur_parent == it->id_from) {
+			for (auto it = Graph<TV, TE>::incidence_list[child].begin(); it != Graph<TV, TE>::incidence_list[child].end(); it++) 
+			{
+				if (cur_parent == it->id_from) 
+				{
 					path.push_front(*it);
 					break;
 				}
@@ -43,7 +52,8 @@ private:
 		}
 		return path;
 	}
-	Path dijkstra(size_t from, size_t to) {
+	Path dijkstra(size_t from, size_t to) 
+	{
 		Path res;
 		sup<TE> inf;
 		std::unordered_map<size_t, bool> checked;
@@ -54,29 +64,37 @@ private:
 		dist.reserve(Graph<TV, TE>::vertex_count);
 		parents.reserve(Graph<TV, TE>::vertex_count);
 		edges.reserve(Graph<TV, TE>::edge_count);
-		for (auto el = Graph<TV, TE>::vertexes.begin(); el != Graph<TV, TE>::vertexes.end(); el++) {
+		for (auto el = Graph<TV, TE>::vertexes.begin(); el != Graph<TV, TE>::vertexes.end(); el++) 
+		{
 			checked[el->second.id] = false;
 			dist[el->second.id] = inf();
 			parents[el->second.id] = 0;
 		}
 		dist[from] = TE();
 		parents[from] = from;
-		for (size_t i = 0; i < Graph<TV, TE>::vertex_count; ++i) {
+		for (size_t i = 0; i < Graph<TV, TE>::vertex_count; ++i) 
+		{
 			size_t v = 0;
-			for (auto it = Graph<TV, TE>::vertexes.cbegin(); it != Graph<TV, TE>::vertexes.cend(); it++) {
-				if (!checked[it->second.id] && (v == 0 || dist[it->second.id] < dist[v])) {
+			for (auto it = Graph<TV, TE>::vertexes.cbegin(); it != Graph<TV, TE>::vertexes.cend(); it++) 
+			{
+				if (!checked[it->second.id] && (v == 0 || dist[it->second.id] < dist[v])) 
+				{
 					v = it->second.id;
 				}
 			}
-			if (dist[v] == inf()) {
+			if (dist[v] == inf()) 
+			{
 				break;
 			}
 			checked[v] = true;
-			for (auto it = Graph<TV, TE>::incidence_list[v].cbegin(); it != Graph<TV, TE>::incidence_list[v].cend(); it++) {
+			for (auto it = Graph<TV, TE>::incidence_list[v].cbegin(); it != Graph<TV, TE>::incidence_list[v].cend(); it++) 
+			{
 				size_t v1 = it->id_to;
-				if (v != v1) {
+				if (v != v1) 
+				{
 					TE len = it->value;
-					if (dist[v] + len < dist[v1]) {
+					if (dist[v] + len < dist[v1]) 
+					{
 						dist[v1] = dist[v] + len;
 						parents[v1] = v;
 					}
@@ -84,12 +102,14 @@ private:
 				}
 			}
 		}
-		if (dist[to] == inf()) {
+		if (dist[to] == inf()) 
+		{
 			return res;
 		}
 		size_t i = to;
 		size_t p = parents[i];
-		while (p != from) {
+		while (p != from) 
+		{
 			res.push_front(p, i, edges[Pair<size_t, size_t>{p, i}]);
 			i = p;
 			p = parents[i];
--- Pair.h
+++ Pair.h
@@ -1,58 +1,76 @@
-#ifndef PAIR_H_LAB3_SEM3
-#define PAIR_H_LAB3_SEM3
 #include <utility>
 #include <iostream>
 
 template <typename First, typename Second>
-struct Pair {
+struct Pair 
+{
 public:
 	First first;
 	Second second;
 public:
 	Pair() = default;
-	explicit Pair(First _first, Second _second) {
+
+	explicit Pair(First _first, Second _second) 
+	{
 		first = _first;
 		second = _second;
 	}
-	Pair(const Pair<First, Second>& another) {
+
+	Pair(const Pair<First, Second>& another) 
+	{
 		first = another.first;
 		second = another.second;
 	}
-	Pair(const std::pair<First, Second>& another) {
+
+	Pair(const std::pair<First, Second>& another) 
+	{
 		first = another.first;
 		second = another.second;
 	}
+
 	~Pair() = default;
-	Pair& operator=(Pair pair) {
+
+	Pair& operator=(Pair pair) 
+	{
 		std::swap(first, pair.first);
 		std::swap(second, pair.second);
 		return *this;
 	}
 };
+
 template <typename First, typename Second>
-bool operator==(const Pair<First, Second>& one, const Pair<First, Second>& two) {
-	if (one.first == two.first && one.second == two.second) {
+bool operator==(const Pair<First, Second>& one, const Pair<First, Second>& two) 
+{
+	if (one.first == two.first && one.second == two.second) 
+	{
 		return true;
 	}
 	return false;
 }
+
 template <typename First, typename Second>
-bool operator!=(const Pair<First, Second>& one, const Pair<First, Second>& two) {
+bool operator!=(const Pair<First, Second>& one, const Pair<First, Second>& two) 
+{
 	return !(one == two);
 }
+
 template <typename First, typename Second>
-std::ostream& operator<<(std::ostream& out, const Pair<First, Second>& pair) {
+std::ostream& operator<<(std::ostream& out, const Pair<First, Second>& pair) 
+{
 	out << "[" << pair.first << ", " << pair.second << "]";
 	return out;
 }
 
 template <typename T>
-struct pair_hash {
-	size_t operator()(const Pair<T, T>& pair) const {
+struct pair_hash 
+{
+	size_t operator()(const Pair<T, T>& pair) const 
+	{
 		std::hash<T> hasher;
 		return hasher(pair.first) + hasher(pair.second);
 	}
 };
+
 template <typename T>
 struct pair_unord_equal 
 {
@@ -65,6 +83,7 @@ struct pair_unord_equal
 		return false;
 	}
 };
+
 template <typename T>
 struct pair_ord_equal 
 {
@@ -77,4 +96,3 @@ struct pair_ord_equal
 		return false;
 	}
 };
-#endif
